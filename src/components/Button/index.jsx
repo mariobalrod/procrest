cd96b62260db1eb3028a939df9fca21dbc537353
Mario <marioballestero.rdg@gmail.com>
2021-01-12 11:07:28 +0100

    commit inicial

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+    return (
+        <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="btn btn-primary">Primary</button>
+    )
+}
+
+export default Button
