2268231ef183358634e7143e379cf0b1de0366c5
espe <esperanza.del.junco@adaits.es>
2021-01-18 23:52:44 +0100

    boton register

---
+++ src/components/ButtonCustom/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+
+const ButtonCustom = (props) => {
+  return <button className="boton">{props.text}</button>;
+};
+
+export default ButtonCustom;
+
