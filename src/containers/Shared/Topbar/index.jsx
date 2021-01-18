3734c3ba337e48376af13ed4c5ab3e191f866296
sergioavilachacon <sergioavilachacon@gmail.com>
2021-01-19 00:28:38 +0100

    Creacion TopBar

---
+++ src/containers/Shared/Topbar/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from "../../../assets/logo.svg";
+import "./styles.css";
+import Button from "../../../components/Button";
+
+function TopBar(props) {
+  return (
+    <nav id="topBar" className="navbar navbar-expand-lg navbar-light">
+
+            <a className="navbar-brand" href="#"><img src={logo} alt=""/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+        <div className="collapse navbar-collapse ml-auto" id="navbarNav">
+                <ul className="navbar-nav ">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Sobre nosotros</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Lugares</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Alojamiento</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contacto</a>
+                    </li>
+                </ul>
+                <div className="botones " >
+                    <li className="nav-item">
+                        <Button type="button" text= "Registro"/>
+                    </li>
+                    <li className="nav-item " id="botonLogin">
+                        <Button type="button" text= "login"/>
+                    </li>
+                </div>
+        </div>
+        
+    </nav>
+  );
+}
+
+export default TopBar;
