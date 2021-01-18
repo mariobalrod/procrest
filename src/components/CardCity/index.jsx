3fd39488c3cd3de9b26ae071daa93fb47e963d74
Mario <marioballestero.rdg@gmail.com>
2021-01-18 11:56:01 +0100

    algunos cambios

--- src/components/CardCity/index.jsx
+++ src/components/CardCity/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./styles.css";
 
-const Card = (props) => {
+const CardCity = (props) => {
   return (
     <div className="container">
-      <img
-        src={props.cityimage}
+      <div
+        style={{background: `url(${props.cityimage}) no-repeat`, backgroundSize: 'cover'}}
         className="card-img-top"
-        alt="cardImage"
       />
       <div className="card-body">
         <h5 className="card-title">{props.citytext}</h5>
@@ -16,4 +15,4 @@ const Card = (props) => {
   );
 };
 
-export default Card;
+export default CardCity;
