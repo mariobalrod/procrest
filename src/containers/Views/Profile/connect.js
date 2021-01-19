8ec78ca08eb83cfca5e344e9277c425a5d10c0d8
Mario <marioballestero.rdg@gmail.com>
2021-01-20 00:34:52 +0100

    add logout

--- src/containers/Views/Profile/connect.js
+++ src/containers/Views/Profile/connect.js
@@ -1,13 +1,25 @@
 import useMe from '../../../data/hooks/useMe';
+import useAuth from '../../../data/hooks/useAuth';
+import { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const useConnect = () => {
+    const { push } = useHistory();
     const { me, isLoading } = useMe();
+    const { logout } = useAuth();
 
     const isAuth = !!me;
+
+    const handleLogout = useCallback(() => {
+      logout();
+      push('/');
+      window.location.reload();
+    }, [logout, push]);
     
     return {
         isAuth,
-        isLoading
+        isLoading,
+        handleLogout
     }
 }
 
