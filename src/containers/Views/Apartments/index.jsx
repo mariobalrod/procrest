f8e6617595d2c17dca491158e3ec36dce47ce7c9
espe <esperanza.del.junco@adaits.es>
2021-01-21 01:53:38 +0100

    vista apartamentos apañada

--- src/containers/Views/Apartments/index.jsx
+++ src/containers/Views/Apartments/index.jsx
@@ -1,12 +1,68 @@
 import React from "react";
-import './styles.css';
-import Carrusel from '../../../components/Carrusel';
+import "./styles.css";
+import Carrusel from "../../../components/Carrusel";
+import CardApartment from "../../../components/CardApartment";
 
 const Apartments = () => {
   return (
-    <div>
+    <div className="apartments">
       <Carrusel />
-      Apartments
+      <div className="grid-container">
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+        <CardApartment
+          className="card"
+          image="https://picsum.photos/200/300"
+          name="Los alcores"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.apartment"
+        />
+      </div>
     </div>
   );
 };
