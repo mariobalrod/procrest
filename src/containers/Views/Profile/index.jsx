1e7c2187cf71defe6c788c0d8735097d8be3a558
sergioavilachacon <sergioavilachacon@gmail.com>
2021-01-22 00:47:05 +0100

    Actualizacion de las cartas, condicional booking y responsive de profile listo

--- src/containers/Views/Profile/index.jsx
+++ src/containers/Views/Profile/index.jsx
@@ -4,8 +4,9 @@ import useConnect from './connect';
 import { Redirect } from "react-router-dom";
 
 import LogoutIcon from '../../../assets/logOut.svg';
-import Avatar from '../../../components/Avatar'
-import CardApartment from '../../../components/CardApartment'
+import Avatar from '../../../components/Avatar';
+import CardApartment from '../../../components/CardApartment';
+import Empty from './Empty.svg'
 
 const Profile = () => {
   const { isAuth, isLoading, handleLogout, me } = useConnect();
@@ -29,64 +30,29 @@ const Profile = () => {
             size="big"
           />
         </div>
-        <h1>Historial de reservas</h1>
-        <div className="cartasPerfil">
-          <div id="fila1">
-            <CardApartment
-              hasButtom={false}
-              name="Casa Palacio"
-              image="https://i.imgur.com/TEfwhA3.jpg"
-              description="Monasterio del s.XVIII completamente remodelado con las máximas comodidades convertido en un lugar único.
-              Capacidad para 20 personas.
-              6 baños completos."
-            />
-            <CardApartment
-              hasButtom={false}
-              name="Nilgar"
-              image="https://i.imgur.com/DAXSN4U.jpg"
-              description="Ideal para 5 personas.
-              2 habitaciones. 
-              Jardín cerrado.
-              Jacuzzi."
-            />
-            <CardApartment
-              hasButtom={false}
-              name="Cabaña Lalo"
-              image="https://i.imgur.com/RF8xFpn.png"
-              description="Piscina-Jacuzzi en una gruta volcánica, climatizada.
-              Capacidad para 8 personas.
-              6 habitaciones con baños propios."
-            />
+        {(me.bookings.length === 0)  ? (
+          
+         <div id="empty">
+            <h1>¿Aun no tiene ninguna reserva?</h1>
+           <img  src={Empty} alt="empty"/>
           </div>
-          <div id="fila2">
-            <CardApartment
-              hasButtom={false}
-              name="Girante"
-              image="https://i.imgur.com/JDgCOjQ.jpg"
-              description="Solárium con hamacas.
-              Zona de hamacas en los jardines.
-              Capacidad para 10 personas.
-              7 baños completos."
-            />
-            <CardApartment
-              hasButtom={false}
-              name="Quitan linda"
-              image="https://imgur.com/hlX0QSn.jpg"
-              description="Espectacular villa situada entre la playa y el monte.
-              Para 10 personas.
-              5 habitaciones dobles.
-              6 baños completos."
-            />
-            <CardApartment
-              hasButtom={false}
-              name="Caloan"
-              image="https://i.imgur.com/LBKlckX.jpg"
-              description="Diseñado para una escapada romántica para 2 personas en un entorno muy privado, tranquilo y hermoso, con vistas de largo alcance.
-              1 habitación.
-              2 baños."
-            />
+        ) : (
+
+          <div id="booking">
+            <h1>Historial de reservas</h1>
+            {me.bookings.map(apartamentBooking =>(
+              <CardApartment
+              key ={apartamentBooking.id}
+              image={apartamentBooking.image}
+              name={apartamentBooking.name}
+              description={apartamentBooking.description}
+              date={apartamentBooking.date}
+              />
+            ))}            
           </div>
-        </div>
+
+        )}
+        
 
       </div>
     </div>
@@ -94,3 +60,22 @@ const Profile = () => {
 };
 
 export default Profile;
+
+/*
+<div className="imgProfile">
+          </div>
+
+<div className="cartasPerfil">
+          <div id="fila1">
+            {apartamentsBooking.map(apartamentBooking =>(
+              <CardApartment
+              key ={apartamentBooking.id}
+              image={apartamentBooking.image}
+              name={apartamentBooking.name}
+              description={apartamentBooking.description}
+              date={apartamentBooking.date}
+              />
+            ))}            
+          </div>
+        </div>
+*/ 
