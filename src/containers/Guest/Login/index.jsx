82bed75264183ff3397959864d6159afcba825c9
Mario <marioballestero.rdg@gmail.com>
2021-01-21 13:37:12 +0100

    responsive modal login register

--- src/containers/Guest/Login/index.jsx
+++ src/containers/Guest/Login/index.jsx
@@ -18,50 +18,52 @@ const Login = () => {
   } = useConnect();
   const { containerProps, indicatorEl } = useLoading({
     loading: true,
-    indicator: <ThreeDots width="100" style={{ color: 'white' }} />,
+    indicator: <ThreeDots width="100" style={{ color: "white" }} />,
   });
 
-  if (isLoading) return <Loader indicator={indicatorEl} containerProps={containerProps} />;
+  if (isLoading)
+    return <Loader indicator={indicatorEl} containerProps={containerProps} />;
 
   return (
     <div className="back">
-      <div className="modal-dialog">
-        <div className="modal-content" style={{ width: "514px" }}>
-          <div className="modal-body containerModal">
-            <div className="headerModal">
-              <img src={Icon} alt="iconLogo" className="iconModal" />
-              <h1 className="titleModal">Inicio de sesión</h1>
-            </div>
-            <form style={{ width: "100%", marginTop: "60px" }} onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <input
-                  placeholder="Email"
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  value={email}
-                  onChange={handleChangeEmail}
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  placeholder="Contraseña"
-                  type="password"
-                  className="form-control"
-                  id="exampleInputPassword1"
-                  value={password}
-                  onChange={handleChangePassword}
-                />
-              </div>
-              <div className="footerModal">
-                <Link to="/register" className="linkB">
-                  ¿No tienes cuenta?
-                </Link>
-                <Button type="submit" text="Iniciar sesión" />
-              </div>
-            </form>
+      <div className="modal-content" style={{ width: "514px" }}>
+        <div className="modal-body containerModal">
+          <div className="headerModal">
+            <img src={Icon} alt="iconLogo" className="iconModal" />
+            <h1 className="titleModal">Inicio de sesión</h1>
           </div>
+          <form
+            style={{ width: "100%", marginTop: "60px" }}
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-4">
+              <input
+                placeholder="Email"
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                value={email}
+                onChange={handleChangeEmail}
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                placeholder="Contraseña"
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                value={password}
+                onChange={handleChangePassword}
+              />
+            </div>
+            <div className="footerModal">
+              <Link to="/register" className="linkB">
+                ¿No tienes cuenta?
+              </Link>
+              <Button type="submit" text="Iniciar sesión" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/containers/Guest/Register/index.jsx
+++ src/containers/Guest/Register/index.jsx
@@ -20,60 +20,59 @@ const Register = () => {
   } = useConnect();
   const { containerProps, indicatorEl } = useLoading({
     loading: true,
-    indicator: <ThreeDots width="100" style={{ color: 'white' }} />,
+    indicator: <ThreeDots width="100" style={{ color: "white" }} />,
   });
 
-  if (isLoading) return <Loader indicator={indicatorEl} containerProps={containerProps} />;
+  if (isLoading)
+    return <Loader indicator={indicatorEl} containerProps={containerProps} />;
 
   return (
     <div className="back">
-      <div className="modal-dialog">
-        <div className="modal-content" style={{ width: "514px" }}>
-          <div className="modal-body containerModal">
-            <div className="headerModal">
-              <img src={Icon} alt="iconLogo" className="iconModal" />
-              <h1 className="titleModal">Registro</h1>
-            </div>
-            <form
-              style={{ width: "100%", marginTop: "60px" }}
-              onSubmit={handleSubmit}
-            >
-               <div className="mb-4">
-                <input
-                  placeholder="Nombre de usuario"
-                  type="text"
-                  className="form-control"
-                  value={username}
-                  onChange={handleChangeUsername}
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  placeholder="Email"
-                  type="email"
-                  className="form-control"
-                  aria-describedby="emailHelp"
-                  value={email}
-                  onChange={handleChangeEmail}
-                />
-              </div>
-              <div className="mb-3">
-                <input
-                  placeholder="Contraseña"
-                  type="password"
-                  className="form-control"
-                  value={password}
-                  onChange={handleChangePassword}
-                />
-              </div>
-              <div className="footerModal">
-                <Link to="/login" className="linkB">
-                  ¿Tienes cuenta?
-                </Link>
-                <Button type="submit" text="Registrarse" />
-              </div>
-            </form>
+      <div className="modal-content" style={{ width: "514px" }}>
+        <div className="modal-body containerModal">
+          <div className="headerModal">
+            <img src={Icon} alt="iconLogo" className="iconModal" />
+            <h1 className="titleModal">Registro</h1>
           </div>
+          <form
+            style={{ width: "100%", marginTop: "60px" }}
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-4">
+              <input
+                placeholder="Nombre de usuario"
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={handleChangeUsername}
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                placeholder="Email"
+                type="email"
+                className="form-control"
+                aria-describedby="emailHelp"
+                value={email}
+                onChange={handleChangeEmail}
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                placeholder="Contraseña"
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={handleChangePassword}
+              />
+            </div>
+            <div className="footerModal">
+              <Link to="/login" className="linkB">
+                ¿Tienes cuenta?
+              </Link>
+              <Button type="submit" text="Registrarse" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
