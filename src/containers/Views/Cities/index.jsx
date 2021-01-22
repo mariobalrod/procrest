d32cc4aaa101314c3612588dc9183acbe3b75833
sergioavilachacon <sergioavilachacon@gmail.com>
2021-01-22 03:16:20 +0100

    searcher eliminado

--- src/containers/Views/Cities/index.jsx
+++ src/containers/Views/Cities/index.jsx
@@ -4,7 +4,6 @@ import useConnect from './connect';
 import CardCity from '../../../components/CardCity'
 import { useLoading, ThreeDots } from '@agney/react-loading';
 import Loader from '../../../components/Loader';
-import Searcher from '../../../components/Searcher'
 
 const Cities = () => {
   const { cities, isLoading } = useConnect();
@@ -22,7 +21,6 @@ const Cities = () => {
       <div className="thumCity">
         <h1 className="presen">Encuentre su ciudad</h1>
       </div>
-      <Searcher />
       <div className="containerCardsCities">
         {cities.map((city) => (
           <CardCity key={city.id} id={city.id} cityimage={city.image} citytext={city.name} />
